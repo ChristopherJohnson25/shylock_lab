9a3efade589063938f2b81c354d029dc8626769c
Chris Johnson <johnsonchris98@gmail.com>
2015-11-04 14:28:10 -0500

    Update Styled

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ console.log("linked")
 
 $(document).ready(function(){
 
+	var $list = ('.history');
+	var $textInput = ('#text_input')
 
+var purse = function (){
+	if ($textInput = 'purse') {
+		$list.text("ShylockBot pulls out his purse containing 10000 ducats.")
+	};
+};
+
+$(document).keypress(purse)
 
 
 
