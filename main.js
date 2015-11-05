d562d5fd27b681a4de5c2784f8e68a4430f97686
Chris Johnson <johnsonchris98@gmail.com>
2015-11-05 08:54:00 -0500

    morning commit

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
 $(document).ready(function(){
+
 	console.log("linked")
-	var ducats = 10000
+	var ducats = 10000;
 	var $list = $('.history');
 	var $textInput = $('#text_input').val();
+	var $ul = $('#log');
+	var loanAmount;
+	var balanceCounter = ducats - loanAmount;
+	console.log(balanceCounter);
 
 	var submit = function(event){
 		var $textInput = $('#text_input').val();
 		if (event.keyCode === 13){
-			var $li = $('<li>');
-			console.log('pressed enter')
+			console.log('pressed enter');
 			console.log($textInput);
+			var $li = $('<li>');
 			if ($textInput === 'purse'){
-				var $li = $('<li>');
-				$('ul').append($li);
-				$li.text('ShylockBot pulls out his purse containing' + " " + ducats + " " + 'ducats.');
+				$ul.append($li);
+				$li.text('ShylockBot pulls out his purse containing' + " " + balanceCounter + " " + 'ducats.');
 				console.log($li);
+				$('#text_input').val("");
+			} else if ($textInput.indexOf('loan')!== -1) {
+				lend(event);
 			}
 		}
 	};
 
+	var lend = function(event){
+		var $textInput = $('#text_input').val();
+		if (event.keyCode === 13){
+			console.log('pressed enter');
+			console.log($textInput);
+			var $li = $('<li>');
+				var loanArray = $textInput.split(" ");
+				loanAmount;
+				for (var i=0; i < loanArray.length; i++) {
+					if(parseInt(loanArray[i])) {
+					loanAmount=parseInt(loanArray[i]);
+					break;
+					}
+					}
+				$ul.append($li);
+				$li.text('ShylockBot lends' + " " + loanAmount + " " + 'ducats');
+				console.log($li);
+				$('#text_input').val("");
+			}
+	}
 
+var balanceCounter=ducats-loanAmount; //THIS IS NOT WORKING!!! //HELP!
 
 $(document).on('keypress', submit);
 
